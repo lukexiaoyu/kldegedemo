ff8b6a150fcd80f6ce7288869406053683219275
lukexiao <814008000@163.com>
2023-09-25 22:19:20 +0800

    1

--- netlify/edge-functions/demo.js
+++ netlify/edge-functions/demo.js
@@ -2,6 +2,8 @@ const peos=[
     {name:'xiaoyu',age:36,sex:'男'},
     {name:'haha',age:5,sex:'女'},
     {name:'茹茹',age:35,sex:'女'},
+    {name:'haiqing',age:34,sex:'女'},
+    {name:'qiaoyu',age:6,sex:'女'},
 ]
 
 
